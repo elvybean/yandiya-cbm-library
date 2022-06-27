fc3dd0353a48429de6e4b0d3cc7c7792aa41e082
elvis <elvisoberoatkins.dev@gmail.com>
2022-06-27 16:59:23 +0100

    minor changes
    renamed cli-app to cli-app-new
    imported cli-app from main

--- .main/cli-app new.py
+++ .main/cli-app new.py
@@ -1,21 +1,26 @@
 """
 Author: Elvis Obero-Atkins
 Last Edited by: Elvis Obero-Atkins
+
 This py script is NOT REQUIRED for yandiya-cbm-library to function
+
 It is CLI python application designed to interact
 with the yandiya-cbm-library.
-Adding calculate cbm of multiple items
 """
 import cbmcalculator
 
 
-def testFunc(IterateStore: list, ErrorDetect: list):
-    """Holds the main logic that accesses the cbmcalculator library
+def listCreate(IterateStore: list, ErrorDetect: list):
+    """Creates a list that contains the extacted excel rows of selected products + item quanities
+
     Args:
-        IterateStore (list): list of ints; the first int stores the cbm & the second int stores the weight
+        IterateStore (list)(2D): (is orginally empty when called from main) stores the extacted excel rows of selected products 
+            + item quanities in a 2 Dimensional list
         ErrorDetect (list): list of ints; the first int counts the errors & the second int counts the number of iterations
+
     Returns:
-        list: stores the calculated cbm and the total weight OR 0 values
+        IterateStore (list)(2D): (is orginally empty when called from main) stores the extacted excel rows of selected products 
+            + item quanities in a 2 Dimensional list
     """
 
     ErrorDetect[1] += 1
@@ -29,34 +34,34 @@ def testFunc(IterateStore: list, ErrorDetect: list):
 
     if inWarehouse == 0:
         ErrorDetect[0] += 1
-        cbm = [0, 0]
         print("\nerror. either incorrect input or item does not exist  ")
     else:
-        cbm = cbmcalculator.calculate(inWarehouse, productQuantity)
-
-    for i in range(len(cbm)):
-        IterateStore[i] += cbm[i]
+        IterateStore.append([inWarehouse, productQuantity])
 
     response = input(
         "\nDo you want to search for another item? y/n  ").capitalize()
 
     if response == "N":
-        IterateStore.append(cbmcalculator.weight_logic(IterateStore[1]))
-        return IterateStore
-
+        if not ErrorDetect[0] > ErrorDetect[1]:
+            return IterateStore
+        else:
+            return 0
     else:
-        return testFunc(IterateStore, ErrorDetect)
+        return listCreate(IterateStore, ErrorDetect)
 
 
 def main():
     """holds the main code of this py file
     Args:
         none
+
     Returns:
         none
     """
 
-    multipleCBM = testFunc([0, 0], [0, 0])
+    listParameter = listCreate([], [0, 0])
+
+    multipleCBM = cbmcalculator.calculate_multiple(listParameter)
 
     print("The Total  CBM is ", multipleCBM[0], ", the total weight is ",
           multipleCBM[1], " the items will be sent in a ", multipleCBM[2])
