a1415f553e2248ad6f2c4bfcf09fb90728e65da9
Elvis Obero-Atkins <elvisoberoatkins.dev@gmail.com>
2022-07-06 11:00:55 +0100

    removed listCreate function from cli-app.py
    moved functionality to main function

--- main/cbmcalculator/__init__.py
+++ main/cbmcalculator/__init__.py
@@ -5,7 +5,6 @@
 #       dimensions as well as weight and cbm & so it doesn't assume the products are malluable.
 # - Implement use of PostgresSQL instead of excel
 
-#from cbmcalculator.main import main
 from cbmcalculator.calculate import calculate
 from cbmcalculator.bin_packing import bin_packing
 from cbmcalculator.search_product import search_product
--- main/cli-app/cli-app.py
+++ main/cli-app/cli-app.py
@@ -21,46 +21,6 @@ sys.path.append(PROJECT_ROOT)
 import cbmcalculator as cbmcalc #########################################################
 #####################################################################################
 
-
-def listCreate(iterateStore: list, ErrorDetect: list):
-    """Creates a list that contains the extacted excel rows of selected products + item quantities
-
-    Args:
-        IterateStore (list): (is orginally empty when called from main) stores the extacted excel rows of selected products 
-            + item quantities in a 2 Dimensional list
-        ErrorDetect (list): list of integer; the first int counts the errors & the second int counts the number of iterations
-
-    Returns:
-        IterateStore (list): stores the extacted excel rows of selected products + item quantities in a 2 Dimensional list
-    """
-
-    ErrorDetect[1] += 1
-
-    parameters = input(
-        "\nWhats the product number, barcode or sku of the item?  ")
-    productQuantity = int(input(
-        "\nWhat's the quantity of the items that you need?  "))
-
-    inWarehouse = cbmcalc.search_product(parameters)
-
-    if inWarehouse == 0:
-        ErrorDetect[0] += 1
-        print("\nerror. either incorrect input or item does not exist  ")
-    else:
-        iterateStore.append([inWarehouse, productQuantity])
-
-    response = input(
-        "\nDo you want to search for another item? y/n  ").capitalize()
-
-    if response == "N":
-        if not ErrorDetect[0] > ErrorDetect[1]:
-            return iterateStore 
-        else:
-            return 0
-    else:
-        return listCreate(iterateStore, ErrorDetect)
-
-
 def main():
     """holds the main code of this py file
     Args:
@@ -75,7 +35,35 @@ def main():
     print(f.read())
     f.close()
 
-    listParameter = listCreate([], [0, 0])
+    listParameter = []
+    ErrorDetect = [0, 0]
+    response = ""
+
+    while response != "N":
+        ErrorDetect[1] += 1
+
+        parameters = input(
+            "\nWhats the product number, barcode or sku of the item?  ")
+        productQuantity = int(input(
+            "\nWhat's the quantity of the items that you need?  "))
+
+        inWarehouse = cbmcalc.search_product(parameters)
+
+        if inWarehouse == 0:
+            ErrorDetect[0] += 1
+            print("\nerror. either incorrect input or item does not exist  ")
+        else:
+            listParameter.append([inWarehouse, productQuantity])
+
+        response = input(
+            "\nDo you want to search for another item? y/n  ").capitalize()
+
+        if response == "N":
+            break
+
+    if listParameter == []:
+        print("\nerror.")
+        return 0
 
     cbm = 0
     weight = 0
@@ -91,7 +79,5 @@ def main():
     shipping = cbmcalc.bin_packing(cbm, weight, binpackParams)
 
     print(cbm, weight, binpackParams, shipping)
-
-    
 
 main()
